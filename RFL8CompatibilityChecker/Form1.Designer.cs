afc800d6c1e4ce20767c841ace59870342eebdb6
RFLEpicGamer <69988411+RFLEpicGamer@users.noreply.github.com>
2021-12-15 23:21:02 -0300

    Add Files

---
+++ RFL8CompatibilityChecker/Form1.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace RFL8CompatibilityChecker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(70, 64);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(627, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Is your pc eligible for rfl8 apps and games?";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(332, 144);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Verify";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(780, 228);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "RFL8Compatibility Checker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ RFL8CompatibilityChecker/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RFL8CompatibilityChecker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            if (Environment.MachineName != "Lenovo")
+            {
+                if (Environment.Is64BitOperatingSystem)
+                {
+                    Definitely();
+                } else
+                {
+                    Maybe();
+                }
+            } else
+            {
+                NotCompatible();
+            }
+            
+        }
+
+        private void NotCompatible()
+        {
+            string title = "Sorry!";
+            string message = "Your pc is not eligible for rfl8apps and games, if you think if this is not yoga device, please contact us at (rfl8team.github.io)";
+            MessageBoxButtons buttons = MessageBoxButtons.OK;
+            MessageBox.Show(message, title, buttons, MessageBoxIcon.Error);
+            label1.Text = "Not eligible for RFL8Apps and Games";
+            label1.ForeColor = Color.Red;
+        }
+
+        private void Maybe()
+        {
+            string title = "Uh...";
+            string message = "Your pc should be able to run RFL8Apps and Games but we don't recommend it because Windows installed in your computer is not 64 bits";
+            MessageBoxButtons buttons = MessageBoxButtons.OK;
+            MessageBox.Show(message, title, buttons, MessageBoxIcon.Warning);
+            label1.Text = "Some rfl8Apps and Games may not run";
+            label1.ForeColor = Color.Yellow;
+        }
+
+        private void Definitely()
+        {
+            string title = "Congratulations!";
+            string message = "Your pc is able to run RFL8Apps and Games with no problem!";
+            MessageBoxButtons buttons = MessageBoxButtons.OK;
+            MessageBox.Show(message, title, buttons, MessageBoxIcon.Information);
+            label1.Text = "Eligible for rfl8Apps and Games";
+            label1.ForeColor = Color.Blue;
+        }
+    }
+}
